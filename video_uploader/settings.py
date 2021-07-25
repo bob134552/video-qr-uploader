94c50b7ce998656149fad51002837d885d7c85bd
bob134552 <bob134552@hotmail.com>
2021-07-25 03:16:01 +0100

    updated settings

--- video_uploader/settings.py
+++ video_uploader/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEVELOPEMENT')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['qrit-video-app.herokuapp.com', 'localhost']
 
 
 # Application definition
